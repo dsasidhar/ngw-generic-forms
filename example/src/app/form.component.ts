9ee357d416fa2edb520bcd6200c081fcbb342321
dsasidhar <sasidhar.dwadasi@gmail.com>
2017-10-29 02:12:34 +0530

    version update and example update

--- example/src/app/form.component.ts
+++ example/src/app/form.component.ts
@@ -3,17 +3,29 @@ import { Validators } from '@angular/forms'
 
 @Component({
   selector: 'form',
+  styles: [`
+  /deep/ field-base{
+    display: block;
+    margin: 10px 2px;
+    border: 1px solid rgba(128, 128, 128, 0.29);
+  }
+  `],
   template: `
   Sample forms
+  <b>Resultant form value</b><i>{{formVar.value | json}}</i>
   <generic-form #formVar [config]="myConfig"></generic-form>
-  <button type="button" (click)="setRandomForm()"> Set random form values</button>
+  <ng-template #myTemplate>
+   This <em>is</em> a stupid template and I want to sit in the middle for no <h4>real</h4>reason
+  </ng-template>
   <br/>
-  {{formVar.value | json}}
   `
 })
 export class FormComponent implements AfterViewInit {
   @ViewChild('formVar')
   private formVar;
+
+  @ViewChild('myTemplate')
+  private templateVar;
 
   setRandomForm() {
     this.formVar.setValues({
@@ -22,6 +34,7 @@ export class FormComponent implements AfterViewInit {
     })
   }
   ngAfterViewInit() {
+    (<any>this.myConfig.children[5]).template = this.templateVar;
     //formVar is available for use here
     console.log(this.formVar);
   }
@@ -47,22 +60,69 @@ export class FormComponent implements AfterViewInit {
         ])]
       },
       {
+        type: 'input',
+        name: 'formFieldName',
+        inputType: 'text',
+        label: 'Change disable status of the form element with the name:'
+      },
+      {
+        type: 'button',
+        buttonText: 'Enable',
+        buttonType: 'button', //just regular button don't trigger form submit
+        onClick: _ => {
+          this.formVar.setDisableStatus(this.formVar.value.formFieldName, false);
+        }
+      },
+      {
+        type: 'button',
+        buttonText: 'Disable',
+        buttonType: 'button', //just regular button don't trigger form submit
+        onClick: _ => {
+          this.formVar.setDisableStatus(this.formVar.value.formFieldName, true);
+        }
+      },
+      {
         type: 'checkbox',
         name: 'doYouAgree',
         value: true,
         label: 'Do you Agree to use this component?'
       },
-      // {
-      //     type: 'template-field',
-      //     name: 'hello',
-      //     template: this.template
-      // },
+      {
+          type: 'template',
+          name: 'hello',
+          template: this.templateVar
+      },
       {
         type: 'select',
         name: 'food',
+        id: 'selectid',
         cssClass: 'select-food',
-        options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
-        placeholder: 'Favourite food'
+        value: "",
+        options: [
+          { label: 'Pizza', value: 4 },
+          { label: 'Samosa', value: 2 },
+          { label: 'Panner', value: 6 },
+          { label: 'Chicken', value: 9 },
+        ],
+        placeholder: 'Favourite food',
+        disablePlaceHolderOption: true
+      },
+      {
+        type: 'textarea',
+        name: 'moreFormElements',
+        value: `{"type":"input","name": "iAmTheNewGuy","inputType": "number","cssClass": "iAmTheNewGuy"}`,
+        label: 'add a valid json here to add it to form'
+      },
+      {
+        type: 'button',
+        buttonType: 'button',
+        buttonText: 'Add to form',
+        onClick: _=>{
+          this.myConfig.children= [
+            ...this.myConfig.children,
+            JSON.parse(this.formVar.value.moreFormElements)
+          ]
+        }
       },
       {
         type: 'radio',
@@ -70,10 +130,10 @@ export class FormComponent implements AfterViewInit {
         cssClass: 'radio-btn',
         value: 3,
         options: [
-          {label: 'label1',value:1},
-          {label: 'label2',value:2},
-          {label: 'label3',value:3},
-          {label: 'label4',value:4}
+          { label: 'label1', value: 1 },
+          { label: 'label2', value: 2 },
+          { label: 'label3', value: 3 },
+          { label: 'label4', value: 4 }
         ],
         label: 'radio label',
         optionClass: 'radio-option-class'
@@ -89,18 +149,26 @@ export class FormComponent implements AfterViewInit {
             cssClass: 'age-holder'
           },
           {
-              type: 'textarea',
-              name: 'bio',
-              placeholder: 'enter your bio',
-              cssClass: 'bio'
+            type: 'textarea',
+            name: 'bio',
+            placeholder: 'enter your bio',
+            cssClass: 'bio'
           },
           {
-            type: 'button',
+            type: 'button', //no button type, this will trigger form submit
             name: 'next',
             buttonText: 'Next',
             cssClass: 'next-btn btn',
             onClick: (event) => {
               console.log('button clicked', event, this);
+            }
+          },
+          {
+            type: 'button',
+            buttonText: 'Set random values',
+            buttonType: 'button', //just regular button don't trigger form submit
+            onClick: _ => {
+              this.setRandomForm();
             }
           }
         ]
